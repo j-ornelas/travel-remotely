c28759cfdd1857f8df1068e82e9583504c6167cf
j-ornelas <johnbryan.ornelas@gmail.com>
2021-01-09 20:16:56 -0500

    add kuala lumpur

--- src/data/cities.ts
+++ src/data/cities.ts
@@ -1,7 +1,19 @@
 import { bangkok } from './bangkok';
+import { dubai } from './dubai';
+import { kualaLumpur } from './kuala-lumpur';
 import { london } from './london';
 import { paris } from './paris';
 import { sanfrancisco } from './sanfrancisco';
+import { singapore } from './singapore';
 import { tokyo } from './tokyo';
 
-export const cities = [bangkok, london, paris, sanfrancisco, tokyo];
+export const cities = [
+  bangkok,
+  dubai,
+  kualaLumpur,
+  london,
+  paris,
+  sanfrancisco,
+  singapore,
+  tokyo,
+];
---
+++ src/data/kuala-lumpur.ts
@@ -0,0 +1,164 @@
+import { City } from './dataTypes';
+
+export const kualaLumpur: City = {
+  name: 'Kuala Lumpur',
+  radio: [
+    {
+      url: '',
+      name: '',
+      description: '',
+    },
+  ],
+  videos: [
+    {
+      url: 'https://www.youtube.com/watch?v=yLTycPjm0nk',
+      time: 'day',
+      method: 'walk',
+      timeStart: 32,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=s91tCVtAYDM',
+      time: 'day',
+      method: 'walk',
+      timeStart: 39,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=s91tCVtAYDM',
+      time: 'day',
+      method: 'walk',
+      timeStart: 39,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=qNIpB8HZo1c',
+      time: 'day',
+      method: 'walk',
+      timeStart: 11,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=ftQ1wIh9bms',
+      time: 'day',
+      method: 'walk',
+      timeStart: 10,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=wzTJZKQKK9U',
+      time: 'night',
+      method: 'walk',
+      timeStart: 12,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=3qIzs9PQLxk',
+      time: 'day',
+      method: 'walk',
+      timeStart: 14,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=nK4Zt5ar2xg',
+      time: 'day',
+      method: 'walk',
+      timeStart: 100,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=V-r2z2XzDcY',
+      time: 'night',
+      method: 'walk',
+      timeStart: 85,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=xOQbdT9f7SM',
+      time: 'night',
+      method: 'walk',
+      timeStart: 1,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=OTL069HYYRQ',
+      time: 'day',
+      method: 'walk',
+      timeStart: 117,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=wWAWA5D0f68',
+      time: 'day',
+      method: 'walk',
+      timeStart: 27,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=DjM88czrBMc',
+      time: 'night',
+      method: 'walk',
+      timeStart: 10,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=Wo2K3soWH_g',
+      time: 'day',
+      method: 'walk',
+      timeStart: 246,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=MeC-eq2knlc',
+      time: 'night',
+      method: 'walk',
+      timeStart: 122,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=iej2lw0bhTM',
+      time: 'day',
+      method: 'walk',
+      timeStart: 119,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=qnXM7J5sCVo',
+      time: 'day',
+      method: 'car',
+      timeStart: 15,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=TxFZ0kacsvs',
+      time: 'night',
+      method: 'car',
+      timeStart: 1,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=OJmKQWwJuSU',
+      time: 'day',
+      method: 'car',
+      timeStart: 167,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=EOxdvaKZk-I',
+      time: 'day',
+      method: 'car',
+      timeStart: 34,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=Y87ySy3dJyo',
+      time: 'day',
+      method: 'train',
+      timeStart: 189,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=Y87ySy3dJyo',
+      time: 'day',
+      method: 'train',
+      timeStart: 189,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=LRAPE1FpjMI',
+      time: 'day',
+      method: 'train',
+      timeStart: 33,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=Bn7Lw_ZVFwQ',
+      time: 'day',
+      method: 'train',
+      timeStart: 33,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=zrN_aS3UjXs',
+      time: 'night',
+      method: 'train',
+      timeStart: 42,
+    },
+  ],
+};
