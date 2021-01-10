050ce6797b4571dc9eb7d73ef16bab35cc1170d7
j-ornelas <johnbryan.ornelas@gmail.com>
2021-01-09 23:04:17 -0500

    add instanbul

--- src/data/cities.ts
+++ src/data/cities.ts
@@ -1,5 +1,6 @@
 import { bangkok } from './bangkok';
 import { dubai } from './dubai';
+import { istanbul } from './istanbul';
 import { kualaLumpur } from './kuala-lumpur';
 import { london } from './london';
 import { newyorkcity } from './newyorkcity';
@@ -11,6 +12,7 @@ import { tokyo } from './tokyo';
 export const cities = [
   bangkok,
   dubai,
+  istanbul,
   kualaLumpur,
   london,
   newyorkcity,
