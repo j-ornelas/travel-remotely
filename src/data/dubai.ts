1e3f0fc0e9efcb721f38d745ae2d7d7a78e20ce0
j-ornelas <johnbryan.ornelas@gmail.com>
2021-01-09 20:16:15 -0500

    add dubai

---
+++ src/data/dubai.ts
@@ -0,0 +1,108 @@
+import { City } from './dataTypes';
+
+export const dubai: City = {
+  name: 'Dubai',
+  radio: [
+    {
+      url:
+        'http://n08.radiojar.com/s1mygd1frxquv?rj-ttl=5&rj-tok=AAABdsrpm14AjoqGGPP1gTOD2w',
+      name: 'Al Arabiya 99',
+      description: 'Arabic Music, News, Talk',
+    },
+    {
+      url: 'https://radio.skynewsarabia.com/stream/radio/skynewsarabia',
+      name: 'Sky News UAE',
+      description: 'News, Talk',
+    },
+    {
+      url: 'https://streaming.positivity.radio/pr/posi60s/icecast.audio',
+      name: 'Best 60s',
+      description: 'Oldies',
+    },
+    {
+      url: 'https://streaming.radionomy.com/JamendoLounge',
+      name: 'Fresh Radio Arabic Dance',
+      description: 'Dance, Club',
+    },
+    {
+      url:
+        'https://n01.radiojar.com/rzcfw4cbsxquv?rj-ttl=5&rj-tok=AAABdsrtWAQAVz7g043c0DXW6A',
+      name: 'Radio Shoma',
+      description: 'Variety, اف ام ریتم قلب های شما با داغترین و محبوبتری',
+    },
+  ],
+  videos: [
+    {
+      url: 'https://www.youtube.com/watch?v=JXe-1dIlCw4',
+      time: 'day',
+      method: 'walk',
+      timeStart: 57,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=6MvMsSsMY8I',
+      time: 'day',
+      method: 'walk',
+      timeStart: 22,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=fKNo4mEpin4',
+      time: 'day',
+      method: 'walk',
+      timeStart: 88,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=0_SAtTIgcws',
+      time: 'night',
+      method: 'walk',
+      timeStart: 17,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=CBKcG3sz6XQ',
+      time: 'night',
+      method: 'walk',
+      timeStart: 19,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=TE2tfavIo3E',
+      time: 'night',
+      method: 'car',
+      timeStart: 600,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=TE2tfavIo3E',
+      time: 'day',
+      method: 'car',
+      timeStart: 27,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=TE2tfavIo3E',
+      time: 'day',
+      method: 'car',
+      timeStart: 27,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=ta6bRf4MqNo',
+      time: 'day',
+      method: 'car',
+      timeStart: 28,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=sJ75Cls5UPs',
+      time: 'day',
+      method: 'train',
+      timeStart: 45,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=ZpS8xt8xTAw',
+      time: 'day',
+      method: 'train',
+      timeStart: 6,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=gGdILj1SKyY',
+      time: 'night',
+      method: 'train',
+      timeStart: 68,
+    },
+  ],
+};
