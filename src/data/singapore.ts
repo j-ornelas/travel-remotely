300ca21af525f815ceb0d8da77e72b4a0c79d467
j-ornelas <johnbryan.ornelas@gmail.com>
2021-01-09 20:16:30 -0500

    add singapore

---
+++ src/data/singapore.ts
@@ -0,0 +1,125 @@
+import { City } from './dataTypes';
+
+export const singapore: City = {
+  name: 'Singapore',
+  radio: [
+    {
+      url: 'https://cdn.istoikona.com/ccnradio/ccnradio/icecast.audio',
+      name: 'CCN',
+      description: 'Chinese Music',
+    },
+    {
+      url:
+        'https://22393.live.streamtheworld.com/YES933_SC?dist=radiosingapore',
+      name: 'YES 933',
+      description: 'Contemporary Hits',
+    },
+    {
+      url: 'https://22283.live.streamtheworld.com/POWER98_LOVESONGS_SC',
+      name: 'Power 98',
+      description: 'Love Songs',
+    },
+    {
+      url: 'https://22283.live.streamtheworld.com/AXRAAC.aac',
+      name: 'AXR Singapore',
+      description: 'Expat Radio, English',
+    },
+    {
+      url: 'https://c16.radioboss.fm:18115/stream',
+      name: '973 FM',
+      description: 'Dance',
+    },
+  ],
+  videos: [
+    {
+      url: 'https://www.youtube.com/watch?v=uZocEYAyUSs',
+      method: 'walk',
+      time: 'day',
+      timeStart: 215,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=sngNOehNZiY',
+      method: 'walk',
+      time: 'night',
+      timeStart: 37,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=WoUp2Gb_D9g',
+      method: 'walk',
+      time: 'day',
+      timeStart: 113,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=uFaW4y4td6c',
+      method: 'walk',
+      time: 'day',
+      timeStart: 56,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=2Bc723aDR0E',
+      method: 'walk',
+      time: 'night',
+      timeStart: 18,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=qlNr55ZnxEE',
+      method: 'walk',
+      time: 'night',
+      timeStart: 607,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=WHX2yx1FD-w',
+      method: 'car',
+      time: 'night',
+      timeStart: 34,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=aUJl46bEWYo',
+      method: 'walk',
+      time: 'day',
+      timeStart: 42,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=IpEpTWIDL4Q',
+      method: 'car',
+      time: 'day',
+      timeStart: 30,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=AEgrx3Uxfts',
+      method: 'car',
+      time: 'day',
+      timeStart: 37,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=SBFFcYssUvI',
+      method: 'train',
+      time: 'day',
+      timeStart: 12,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=SBFFcYssUvI',
+      method: 'train',
+      time: 'night',
+      timeStart: 12,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=NrBThk2mmTE',
+      method: 'train',
+      time: 'day',
+      timeStart: 9,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=NrBThk2mmTE',
+      method: 'train',
+      time: 'night',
+      timeStart: 9,
+    },
+    {
+      url: 'https://www.youtube.com/watch?v=C2A5i3pd63o',
+      method: 'train',
+      time: 'day',
+      timeStart: 141,
+    },
+  ],
+};
